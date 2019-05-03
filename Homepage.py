939e9e12035412afce9c8d6d81908fcdcbaf6f8c
HarhiniS <harini15122000@gmail.com>
2019-05-03 09:27:05 +0530

    practice

--- Homepage.py
+++ Homepage.py
@@ -10,7 +10,7 @@ def student():
 @app.route('/next', methods=['post'])
 def next():
     form_dict = request.form
-    if 'login' in form_dict:
+    if 'submit' in form_dict:
         return render_template('login.html')
     else:
         return render_template('register.html')
